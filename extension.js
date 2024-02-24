204f8986f55e03830497f146ec9584c8ce746c9b
sakshamgupta <catchgsaksham@gmail.com>
2024-02-23 23:24:55 -0600

    start stop function structure

--- extension.js
+++ extension.js
@@ -2,13 +2,25 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+function startRecording() {
+	vscode.window.showInformationMessage('Starting Audio');
+	
+}
+
+function stopRecording() {
+	vscode.window.showInformationMessage('Stopping Audio');
+	
+}
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+
+	let started = false;
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -17,18 +29,32 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hackillinois.helloWorld', function () {
+	let start = vscode.commands.registerCommand('hackillinois.start', function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from hackillinois!');
+		if (!started) {
+			console.log("HERE")
+			startRecording();
+			started = true;
+		}
+
 	});
 
-	context.subscriptions.push(disposable);
+	let stop = vscode.commands.registerCommand('hackillinois.stop', function () {
+		// The code you place here will be executed every time your command is executed
+		if (started) {
+			// Display a message box to the user
+			stopRecording();
+			started = false;
+		}
+
+	});
+
+	context.subscriptions.push(start, stop);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
